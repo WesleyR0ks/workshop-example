73e29d57ede13ea84254baa87b0785d0b8017dfd
Maurice Snoeren <mac.snoeren@avans.nl>
2021-02-10 10:13:56 +0100

    Added method mul.

--- src/com/avans/Main.java
+++ src/com/avans/Main.java
@@ -5,9 +5,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Students!");
         System.out.println(sum(12, 12));
+        System.out.println(mul(12, 12));
     }
 
     public static int sum( int a, int b ) {
         return a+b;
+    }
+
+    public static int mul (int a, int b) {
+        return a*b;
     }
 }
