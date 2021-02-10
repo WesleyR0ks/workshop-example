a071e97280671d68f6fdf18a855025216c3ac106
Maurice Snoeren <mac.snoeren@avans.nl>
2021-02-10 10:07:37 +0100

    Added method sum(int,int)

--- src/com/avans/Main.java
+++ src/com/avans/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Students!");
-		System.out.println("Berend was here!");
+        System.out.println(sum(12, 12));
+    }
+
+    public static int sum( int a, int b ) {
+        return a+b;
     }
 }
